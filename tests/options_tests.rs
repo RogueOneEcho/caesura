812ac56e291d64c9a38e958257bcd4e51be10e60
RogueOneEcho <Um9ndWVPbmVFY2hv@pm.me>
2024-10-13 19:29:32 +0100

    ci: improve tests

--- tests/options_tests.rs
+++ tests/options_tests.rs
@@ -1,36 +1,108 @@
-use caesura::errors::AppError;
-use caesura::logging::{Trace, Logger};
-use caesura::options::{BatchOptions, FileOptions, Options, OptionsProvider, RunnerOptions, SharedOptions, SpectrogramOptions, TargetOptions, UploadOptions, VerifyOptions};
-use caesura::testing::*;
+use caesura::logging::{Trace, Logger, Verbosity};
+use caesura::options::*;
+
+const LOG_LEVEL: Verbosity = Trace;
 
 #[tokio::test]
-async fn validate_options() -> Result<(), AppError> {
+async fn batch_options_validate() {
     // Arrange
-    Logger::init_new(Trace);
+    Logger::init_new(LOG_LEVEL);
     let provider = OptionsProvider::new();
-    let shared_options = TestOptionsFactory::from_with_env(SharedOptions {
-        verbosity: Some(Trace),
-        ..SharedOptions::default()
-    });
-    
+
     // Act
     let batch_options = provider.get::<BatchOptions>();
+
+    // Assert
+    assert!(batch_options.validate());
+}
+
+#[tokio::test]
+async fn file_options_validate() {
+    // Arrange
+    Logger::init_new(LOG_LEVEL);
+    let provider = OptionsProvider::new();
+
+    // Act
     let file_options = provider.get::<FileOptions>();
+
+    // Assert
+    assert!(file_options.validate());
+}
+
+#[tokio::test]
+async fn runner_options_validate() {
+    // Arrange
+    Logger::init_new(LOG_LEVEL);
+    let provider = OptionsProvider::new();
+
+    // Act
     let runner_options = provider.get::<RunnerOptions>();
+
+    // Assert
+    assert!(runner_options.validate());
+}
+
+#[tokio::test]
+async fn spectrogram_options_validate() {
+    // Arrange
+    Logger::init_new(LOG_LEVEL);
+    let provider = OptionsProvider::new();
+
+    // Act
     let spectrogram_options = provider.get::<SpectrogramOptions>();
+
+    // Assert
+    assert!(spectrogram_options.validate());
+}
+
+#[tokio::test]
+async fn target_options_validate() {
+    // Arrange
+    Logger::init_new(LOG_LEVEL);
+    let provider = OptionsProvider::new();
+
+    // Act
     let target_options = provider.get::<TargetOptions>();
+
+    // Assert
+    assert!(target_options.validate());
+}
+
+#[tokio::test]
+async fn upload_options_validate() {
+    // Arrange
+    Logger::init_new(LOG_LEVEL);
+    let provider = OptionsProvider::new();
+
+    // Act
     let upload_options = provider.get::<UploadOptions>();
-    let verify_options = provider.get::<VerifyOptions>();    
+
+    // Assert
+    assert!(upload_options.validate());
+}
+
+#[tokio::test]
+async fn verify_options_validate() {
+    // Arrange
+    Logger::init_new(LOG_LEVEL);
+    let provider = OptionsProvider::new();
+
+    // Act
+    let verify_options = provider.get::<VerifyOptions>();
+
+    // Assert
+    assert!(verify_options.validate());
+}
+
+#[tokio::test]
+async fn shared_options_validate() {
+    // Arrange
+    Logger::init_new(LOG_LEVEL);
+    let provider = OptionsProvider::new();
+
+    // Act
+    let shared_options = provider.get::<SharedOptions>();
 
     // Assert
     assert!(shared_options.validate());
-    assert!(batch_options.validate());
-    assert!(file_options.validate());
-    assert!(runner_options.validate());
-    assert!(spectrogram_options.validate());
-    assert!(target_options.validate());
-    assert!(upload_options.validate());
-    assert!(verify_options.validate());
-    
-    Ok(())
 }
