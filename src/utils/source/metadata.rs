d361e76bfb50e78da8b404866e31b3eb852b3ced
RogueOneEcho <Um9ndWVPbmVFY2hv@pm.me>
2025-01-15 17:12:19 +0000

    feat: use composer for name instead of `Various Artists` or `Unknown Artist` (#128)

--- src/utils/source/metadata.rs
+++ src/utils/source/metadata.rs
@@ -39,16 +39,30 @@ fn get_artist(group: &Group) -> String {
         info.artists
     } else if info.dj.len() == 1 {
         debug!(
-            "Source has {} artists so using DJ in name",
+            "Source has {} main artists so using DJ in name",
             info.artists.len()
         );
         info.dj
+    } else if info.composers.len() == 1 {
+        debug!(
+            "Source has {} main artists so using composer in name",
+            info.artists.len()
+        );
+        info.composers
     } else if info.artists.is_empty() {
-        warn!("Unable to determine a suitable artist for name. Defaulting to `{UNKNOWN_ARTIST}` which likely isn't ideal");
-        return UNKNOWN_ARTIST.to_owned();
+        if info.with.is_empty() {
+            warn!("Source has no main or guest artists. Defaulting to `{UNKNOWN_ARTIST}` which likely isn't ideal");
+            return UNKNOWN_ARTIST.to_owned();
+        } else if info.with.len() <= MAX_ARTISTS {
+            warn!("Source has no main artist so using guest artists in name");
+            info.with
+        } else {
+            debug!("Source has no main artist and {} guest artists so name will be condensed as `{VARIOUS_ARTISTS}`", info.with.len());
+            return VARIOUS_ARTISTS.to_owned();
+        }
     } else {
         debug!(
-            "Source has {} artists so name will be condensed as `{VARIOUS_ARTISTS}`",
+            "Source has {} main artists so name will be condensed as `{VARIOUS_ARTISTS}`",
             info.artists.len()
         );
         return VARIOUS_ARTISTS.to_owned();
@@ -205,6 +219,100 @@ mod tests {
                 dj: vec![Credit {
                     id: 12345,
                     name: "DJ One".to_owned(),
+                }],
+                ..Credits::default()
+            }),
+            ..Group::default()
+        };
+
+        // Act
+        let artist = get_artist(&group);
+
+        // Assert
+        assert_eq!(artist, expected);
+    }
+
+    #[test]
+    fn get_artist_composer() {
+        // Arrange
+        let expected = "Composer One".to_owned();
+        let group = Group {
+            music_info: Some(Credits {
+                artists: vec![
+                    Credit {
+                        id: 12345,
+                        name: "Artist One".to_owned(),
+                    },
+                    Credit {
+                        id: 12345,
+                        name: "Artist Two".to_owned(),
+                    },
+                    Credit {
+                        id: 12345,
+                        name: "Artist Three".to_owned(),
+                    },
+                ],
+                composers: vec![Credit {
+                    id: 12345,
+                    name: "Composer One".to_owned(),
+                }],
+                ..Credits::default()
+            }),
+            ..Group::default()
+        };
+
+        // Act
+        let artist = get_artist(&group);
+
+        // Assert
+        assert_eq!(artist, expected);
+    }
+
+    #[test]
+    fn get_artist_and_guest() {
+        // Arrange
+        let expected = VARIOUS_ARTISTS.to_owned();
+        let group = Group {
+            music_info: Some(Credits {
+                artists: vec![
+                    Credit {
+                        id: 12345,
+                        name: "Artist One".to_owned(),
+                    },
+                    Credit {
+                        id: 12345,
+                        name: "Artist Two".to_owned(),
+                    },
+                    Credit {
+                        id: 12345,
+                        name: "Artist Three".to_owned(),
+                    },
+                ],
+                with: vec![Credit {
+                    id: 12345,
+                    name: "Guest One".to_owned(),
+                }],
+                ..Credits::default()
+            }),
+            ..Group::default()
+        };
+
+        // Act
+        let artist = get_artist(&group);
+
+        // Assert
+        assert_eq!(artist, expected);
+    }
+
+    #[test]
+    fn get_artist_guest_only() {
+        // Arrange
+        let expected = "Guest One".to_owned();
+        let group = Group {
+            music_info: Some(Credits {
+                with: vec![Credit {
+                    id: 12345,
+                    name: "Guest One".to_owned(),
                 }],
                 ..Credits::default()
             }),
